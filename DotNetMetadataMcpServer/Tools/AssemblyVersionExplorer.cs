391d741dc1eb4aed7f3a1cc5e4dd37965ea0bc4f
Abdelkader Afifi <216317517+Abdelkader-Afifi@users.noreply.github.com>
2026-07-08 08:46:16 +0300

    Add AssemblyVersionExplorer Tool returns the version of any assembly

---
+++ DotNetMetadataMcpServer/Tools/AssemblyVersionExplorer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetMetadataMcpServer.Tools
+{
+    public class AssemblyVersionExplorer
+    {
+        public string GetAssemblyVersion(string assemblyPath)
+        {
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly.GetName().Version.ToString();
+        }
+    }
+}
